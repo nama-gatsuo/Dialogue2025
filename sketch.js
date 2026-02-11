37c81b51ec89e2498eae6c67647c3ac9d5034a72
Ayumu Nagamatsu <ayumu.nagamatsu@gmail.com>
2026-02-11 10:59:16 +0900

    added recording function

--- sketch.js
+++ sketch.js
@@ -1,18 +1,34 @@
 // template is from https://github.com/aferriss/p5jsShaderExamples/tree/gh-pages
 
 let uniformsShader;
-let smallFBO, largeFBO;
+let smallFBO;
 let aspectRatio;
 let randonclrpos = [];
 let randomGauss = [];
 let lensType;
+let canvas;
+
+// render / display settings
+// fixed render resolution (display will be half size)
+let renderWidth = 1080 * 2;
+let renderHeight = 1920 * 2;
+let displayScaleFromRender = 0.5;
+let exportScale = 1; // additional scale for export
+
+// frame export
+let exportFBO = null;
+let isRecording = false;
+let recordFrameIndex = 0;
+let recordFps = 30;
+let recordEvery = 1;
+let recordEndFrame = 300; // stop after this many exported frames
 
 function preload() {
   uniformsShader = loadShader("uniform.vert", "uniform.frag");
 }
 
 function setup() {
-  pixelDensity(window.devicePixelRatio || 2);
+  pixelDensity(1);
 
   // random color(combo1&combo2) every time
   for (let i = 0; i < 6; i++) {
@@ -27,17 +43,17 @@ function setup() {
   resultString = randonclrpos.map(x => ['r','g','b'][x]).join('');
   console.log(randonclrpos);
 
-  let canvasWidth = windowWidth * 0.9;
-  let canvasHeight = windowHeight * 0.9;
+  let canvasWidth = Math.floor(renderWidth * displayScaleFromRender);
+  let canvasHeight = Math.floor(renderHeight * displayScaleFromRender);
 
-  createCanvas(canvasWidth, canvasHeight, WEBGL);
+  canvas = createCanvas(renderWidth, renderHeight, WEBGL);
+  setCanvasDisplaySize(canvasWidth, canvasHeight);
 
-  aspectRatio = canvasWidth / canvasHeight;
+  aspectRatio = renderWidth / renderHeight;
 
-  // 2x resolution for FBO
   smallFBO = createFramebuffer({
-    width: canvasWidth * 2,
-    height: canvasHeight * 2,
+    width: renderWidth,
+    height: renderHeight,
     format: UNSIGNED_BYTE,
     density: 1,
   });
@@ -49,19 +65,28 @@ function setup() {
 
 function draw() {
 
-  renderToFBO(smallFBO);
+  const time = isRecording ? (recordFrameIndex / recordFps) : (millis() / 1000);
+  renderToFBO(smallFBO, time);
 
   background(0);
   imageMode(CENTER);
 
   image(smallFBO, 0, 0, width, height);
 
-  if (frameCount > 10000) {
-    window.location.reload();
+  if (isRecording && frameCount % recordEvery === 0) {
+    exportFrame(time);
+    recordFrameIndex += 1;
+    if (recordFrameIndex >= recordEndFrame) {
+      isRecording = false;
+    }
   }
+
+  // if (frameCount > 10000) {
+  //   window.location.reload();
+  // }
 }
 
-function renderToFBO(fbo) {
+function renderToFBO(fbo, time) {
   fbo.begin();
 
   // Clear the FBO
@@ -71,7 +96,7 @@ function renderToFBO(fbo) {
   shader(uniformsShader);
 
   // Send uniforms to shader
-  uniformsShader.setUniform("time", millis() / 1000);
+  uniformsShader.setUniform("time", time);
   uniformsShader.setUniform("width", fbo.width);
   uniformsShader.setUniform("height", fbo.height);
   uniformsShader.setUniform("rand", randonclrpos);
@@ -90,20 +115,62 @@ function renderToFBO(fbo) {
 }
 
 function windowResized() {
-  let w = windowWidth;
-  let h = windowHeight;
-  resizeCanvas(w, h);
-  aspectRatio = w / h;
+  let w = Math.floor(renderWidth * displayScaleFromRender);
+  let h = Math.floor(renderHeight * displayScaleFromRender);
+  setCanvasDisplaySize(w, h);
+}
 
-  if (smallFBO) smallFBO.remove();
-  smallFBO = createFramebuffer({
-    width: w * 2,
-    height: h * 2,
+function setCanvasDisplaySize(w, h) {
+  if (!canvas || !canvas.elt) return;
+  canvas.elt.style.width = `${w}px`;
+  canvas.elt.style.height = `${h}px`;
+  canvas.elt.style.position = "absolute";
+  canvas.elt.style.left = "50%";
+  canvas.elt.style.top = "50%";
+  canvas.elt.style.transform = "translate(-50%, -50%)";
+}
+
+function ensureExportFBO() {
+  if (!smallFBO) return null;
+  let exportW = Math.floor(smallFBO.width * exportScale);
+  let exportH = Math.floor(smallFBO.height * exportScale);
+  if (exportFBO && exportFBO.width === exportW && exportFBO.height === exportH) {
+    return exportFBO;
+  }
+  if (exportFBO) exportFBO.remove();
+  exportFBO = createFramebuffer({
+    width: exportW,
+    height: exportH,
     format: UNSIGNED_BYTE,
     density: 1,
   });
+  return exportFBO;
 }
 
+function exportFrame(time) {
+  let fbo = ensureExportFBO();
+  if (!fbo) return;
+
+  fbo.begin();
+  clear();
+  shader(uniformsShader);
+  uniformsShader.setUniform("time", time);
+  uniformsShader.setUniform("width", float(fbo.width));
+  uniformsShader.setUniform("height", float(fbo.height));
+  uniformsShader.setUniform("rand", randonclrpos);
+  uniformsShader.setUniform("gauss", randomGauss);
+  uniformsShader.setUniform("lensType", lensType);
+
+  push();
+  scale(1, -1);
+  rect(-fbo.width / 2, -fbo.height / 2, fbo.width, fbo.height);
+  pop();
+  fbo.end();
+
+  let img = fbo.get();
+  let filename = `frame_${String(recordFrameIndex).padStart(5, "0")}`;
+  img.save(filename, "png");
+}
 
 
 //------------------------------------
@@ -111,37 +178,15 @@ function windowResized() {
 
 function keyPressed() {
   if (key === 's' || key === 'S') {
+    const time = millis() / 1000;
+    exportFrame(time);
+  }
 
-    let exportSize_w = 1920*2;
-    let exportSize_h = 1080*2;
-
-    let exportFBO = createFramebuffer({
-      width: exportSize_w,
-      height: exportSize_h,
-      format: UNSIGNED_BYTE,
-      density: 1,
-    });
-
-    exportFBO.begin();
-    clear();
-    shader(uniformsShader);
-    uniformsShader.setUniform("time", millis() / 1000);
-    uniformsShader.setUniform("width", float(exportSize_w));
-    uniformsShader.setUniform("height", float(exportSize_h));
-    uniformsShader.setUniform("rand", randonclrpos);
-    uniformsShader.setUniform("gauss", randomGauss);
-    uniformsShader.setUniform("lensType", lensType);
-    
-    push();
-    scale(1, -1);
-    rect(-exportSize_w / 2, -exportSize_h / 2, exportSize_w, exportSize_h);
-    pop();
-    exportFBO.end();
-
-    let img = exportFBO.get();
-    img.save('output_highres', 'png');
-
-    exportFBO.remove();
+  if (key === 'r' || key === 'R') {
+    isRecording = !isRecording;
+    if (isRecording) {
+      recordFrameIndex = 0;
+    }
   }
 }
 
